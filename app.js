b0b76208e76bb3fc296539bfc805ff1457d19d0c
David Cosgrove <dcosgrove@mecommerce.co>
2015-06-16 15:59:24 -0700

    foundations

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-
-
-
 var express = require('express');
 var app = express();
-
 
 var config = {
 	port: process.env.PORT || 8000
 };
 
-//app.use('/', router);
-
-// routes.connect(router);
+app.use('/', function(req, res, next) {
+	res.json({
+		j9dl: 'forthcoming'
+	});
+});
 
 app.listen(config.port, function(){
-	logger.success({
-		message: 'server started on port ' + config.port
-	});
+	console.log('server started on port', config.port);
 });
