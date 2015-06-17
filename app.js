ef13b2d5ccf9458a51e7ad911bcb2a4fda9aee83
David Cosgrove <dcosgrove@mecommerce.co>
2015-06-16 17:17:02 -0700

    basic foundations

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+
+var Promise = require('bluebird');
 var express = require('express');
-var app = express();
+var router = express.Router();
+var mongoose = require('mongoose');
+Promise.promisifyAll(require('mongoose'));
 
 var config = {
-	port: process.env.PORT || 8000
+	port: process.env.PORT || 8000,
+	mongoUrl: 'mongodb://localhost:27017/j9dl'
 };
 
-app.use('/', function(req, res, next) {
-	res.json({
-		j9dl: 'forthcoming'
+mongoose.connect(config.mongoUrl);
+
+var dbPromise = new Promise(function(resolve, reject) {
+
+	mongoose.connection.on('error', function() {
+		console.log('error connecting mongoose');
+		reject();
+	});
+
+	mongoose.connection.once('open', function() {
+		console.log('Mongoose connection established');
+		global.db = mongoose;
+		resolve();
 	});
 });
 
-app.listen(config.port, function(){
-	console.log('server started on port', config.port);
+dbPromise.then(function() {
+
+	var app = express();
+	var router = express.Router();
+
+	app.use('/', router);
+
+	app.listen(config.port, function(){
+		console.log('server started on port', config.port);
+	});
+
 });
+
+
+
