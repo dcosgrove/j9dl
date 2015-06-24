94215cd356e4720e49ef8204d2dc8ab58dc1902b
David Cosgrove <dcosgrove@mecommerce.co>
2015-06-23 18:01:32 -0700

    more trueskill functions

--- server/modules/games/trueskill/index.js
+++ server/modules/games/trueskill/index.js
@@ -39,4 +39,104 @@ var calculateMatchQuality = function(game) {
     );
 
     return expPart * sqrtPart;
+};
+
+// public override IDictionary<TPlayer, Rating> CalculateNewRatings<TPlayer>(GameInfo gameInfo,
+//                                                                           IEnumerable
+//                                                                               <IDictionary<TPlayer, Rating>>
+//                                                                               teams, params int[] teamRanks)
+// {
+//     Guard.ArgumentNotNull(gameInfo, "gameInfo");
+//     ValidateTeamCountAndPlayersCountPerTeam(teams);
+
+//     RankSorter.Sort(ref teams, ref teamRanks);
+
+//     IDictionary<TPlayer, Rating> team1 = teams.First();
+//     IDictionary<TPlayer, Rating> team2 = teams.Last();
+
+//     bool wasDraw = (teamRanks[0] == teamRanks[1]);
+
+//     var results = new Dictionary<TPlayer, Rating>();
+
+//     UpdatePlayerRatings(gameInfo,
+//                         results,
+//                         team1,
+//                         team2,
+//                         wasDraw ? PairwiseComparison.Draw : PairwiseComparison.Win);
+
+//     UpdatePlayerRatings(gameInfo,
+//                         results,
+//                         team2,
+//                         team1,
+//                         wasDraw ? PairwiseComparison.Draw : PairwiseComparison.Lose);
+
+//     return results;
+// }
+
+var calculateNewRatings = function(game, teams) {
+
+    var team1 = teams[0];
+    var team2 = teams[1];
+
+    var results;
+}
+
+
+
+var calculateStakes = function(game, teams) {
+
+    var drawMargin = getDrawMarginFromDrawProbability(game.drawProbability, game.beta);
+    var betaSquared = math.square(game.beta);
+    var tauSquared = math.square(game.dynamicsFactor);
+
+    var teamA = {
+        players: teams[0],
+        meanSum: _.sum(teams[0], function(player) { return player.rating.mean; }),
+        stdDevSum = _.sum(teams[0], function(player) { return player.rating.standardDeviation; })
+    };
+
+    var teamB = {
+        players: teams[1],
+        meanSum: _.sum(teams[1], function(player) { return player.rating.mean; }),
+        stdDevSum = _.sum(teams[1], function(player) { return player.rating.standardDeviation; })
+    };    
+
+    var totalPlayers = teamA.players.length + teamB.players.length;
+
+    var c = math.sqrt( teamA.stdDevSum + teamB.stdDevSum + (totalPlayers * betaSquared) );
+
+    var getStakesForOutcome = function(winner, loser) {
+
+        var meanDelta = winner.meanSum - loser.meanSum;
+
+        var v = TruncatedGaussianCorrectionFunctions.VExceedsMargin(meanDelta, drawMargin, c);
+        var w = TruncatedGaussianCorrectionFunctions.WExceedsMargin(meanDelta, drawMargin, c);
+
+        var calculateNewRatingsForPlayer = function(player) {
+            var oldRating = player.rating;
+
+            var meanMultiplier = (math.square(oldRating.standardDeviation) + tauSquared) / c;
+            var stdDevMultiplier = (math.square(oldRating.standardDeviation) + tauSquared) / math.square(c);
+
+            var playerMeanDelta = meanMultiplier * v * rankMultiplier;
+            var newMean = oldRating.mean + playerMeanDelta;
+
+            var newStdDev = math.sqrt((math.square(oldRating.standardDeviation) + tauSquared ) * ( 1 - w * stdDevMultiplier ));
+
+            return {
+                mean: newMean,
+                standardDeviation: newStdDev
+            }
+        });
+
+        return {
+            winner: _.map(winner.players, calculateNewRatingsForPlayer),
+            loser: _.map(loser.players, calculateNewRatingsForPlayer)
+        }
+    };
+
+    return {
+        teamAWins: getStakesForOutcome(teamA, teamB),
+        teamBWins: getStakesForOutcome(teamB, teamA)
+    }
 }
