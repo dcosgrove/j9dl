c7c874da8fb7af3af208c6d72bb9937ffa6636b7
David Cosgrove <dcosgrove@mecommerce.co>
2015-06-26 17:28:08 -0700

    matchmaking

--- server/modules/games/matchmaking/index.js
+++ server/modules/games/matchmaking/index.js
@@ -2,8 +2,14 @@
 module.exports = function(compare) {
 
 	return {
-		findBalancedTeams: function(game, teams) {
+		findBalancedTeams: function(game) {
+			var players = game.players;
 
+			var teamSize = players.length / 2;
+
+			for(var i = 0; i < teamSize; i++) {
+				
+			}
 		}
 	}
 }
