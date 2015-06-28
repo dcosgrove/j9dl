ff61954ef82d9e8d2c5ecfda9e044b5d81a1aad9
david <davidmcosgrove@gmail.com>
2015-06-28 01:58:21 -0700

    implemented team balancer

--- server/modules/games/matchmaking/index.js
+++ server/modules/games/matchmaking/index.js
@@ -1,15 +1,40 @@
+var Combinatorics = require('js-combinatorics');
+var _ = require('lodash');
 
-module.exports = function(compare) {
+module.exports = {
 
-	return {
-		findBalancedTeams: function(game) {
+	findBalancedTeams: function(compare) {
+		
+		return function(game) {
+		
 			var players = game.players;
 
 			var teamSize = players.length / 2;
 
-			for(var i = 0; i < teamSize; i++) {
+			var generator = Combinatorics.combination(players, teamSize);
+
+			var combination;
+
+			var maxQuality = 0;
+			var finalTeams;
+			
+			while(combination = generator.next()) {
+
+				var teamA = combination;
+				var teamB = _.xor(players, teamA);
+
+				var quality = compare(game, [teamA, teamB]);
 				
+				if(quality >= maxQuality) {
+					maxQuality = quality;
+					finalTeams = [ teamA, teamB ];
+				}
 			}
+		
+			return {
+				teams: finalTeams,
+				quality: maxQuality
+			};
 		}
 	}
 }
